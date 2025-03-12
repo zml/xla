5ae7c57db92cedd196417dbbda7e756f127bf1ae
Steeve Morin <steeve@zml.ai>
2025-03-12 15:05:58 +0000

    [ROCm] Enable mfma instructions by passing the correct arch name
    Without this commit, mfma instructions would not be emitted
    by this pass.

--- xla/backends/gpu/codegen/triton/compilation_pipeline_rocm.cc
+++ xla/backends/gpu/codegen/triton/compilation_pipeline_rocm.cc
@@ -89,7 +89,7 @@ absl::Status CreateTritonPipeline(
   pm->addPass(mt::gpu::createTritonGPUCoalesce());
   pm->addPass(mt::gpu::createTritonGPURemoveLayoutConversions());
   pm->addPass(mt::gpu::createTritonGPUOptimizeThreadLocality());
-  pm->addPass(mlir::createTritonAMDGPUAccelerateMatmulPass());
+  pm->addPass(mlir::createTritonAMDGPUAccelerateMatmulPass(cc.gfx_version()));
   pm->addPass(mt::gpu::createTritonGPURemoveLayoutConversions());
   // TODO ROCm Check if we want to compare MI100 and greater
   pm->addPass(mlir::createTritonAMDGPUOptimizeEpiloguePass());
