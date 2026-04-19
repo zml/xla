581e6dba590fd4b77946bdc0eb4e70f22c4e9382
Benjamin Rabier <benjamin@zml.ai>
2026-04-19 17:19:36 +0000

    [xla:gpu] Take shared lock on GPU while unloading the CUDA modules
    This is an improvement over the previous commit regarding the parallel
    compilation with PJRT_Client_Compile. Similar to `cuMemcpyDtoHAsync_`
    `cuModuleUnload` blocks and prevents the thread that launched the
    DelayKernel to finish the profiling setup with its CUDA calls being
    also blocked now. It's only unblocked by the DelayKernel timing out.

--- xla/service/gpu/gpu_executable.h
+++ xla/service/gpu/gpu_executable.h
@@ -422,6 +422,12 @@ class GpuExecutable : public Executable {
   // executable is destroyed.
   absl::flat_hash_map<stream_executor::StreamExecutor*, se::ScopedModuleHandle>
       module_handles_ ABSL_GUARDED_BY(module_handle_mutex_);
+
+  // Set of executors this executable has been run on. Used in the destructor
+  // to take reader locks on GetGpuMutex, preventing cuModuleUnload from racing
+  // with the DelayKernel used during autotuning profiling.
+  absl::flat_hash_set<stream_executor::StreamExecutor*> executors_
+      ABSL_GUARDED_BY(module_handle_mutex_);
   // Cache of constant buffer allocation maps used by `ResolveConstantGlobals`.
   absl::flat_hash_map<stream_executor::StreamExecutor*,
                       std::unique_ptr<BufferAllocToDeviceMemoryMap>>
