5e5f32056bb2c456a4694af4a9492c8b4e5f1bc2
Reese Chong <dev.reese.chong@gmail.com>
2026-07-20 12:55:18 +0000

    [XLA:GPU] register Vulkan platform and enumerate devices

---
+++ xla/stream_executor/vulkan/vulkan_platform_id.cc
@@ -0,0 +1,26 @@
+/* Copyright 2026 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/stream_executor/vulkan/vulkan_platform_id.h"
+
+#include "xla/stream_executor/platform.h"
+
+namespace stream_executor::vulkan {
+
+PLATFORM_DEFINE_ID(kVulkanPlatformId, VULKAN);
+
+}  // namespace stream_executor::vulkan
+
+
