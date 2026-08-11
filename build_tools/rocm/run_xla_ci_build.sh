5147dd650ee50af6608861bb5464c60afba7de41
Milica Makevic <Milica.Makevic@amd.com>
2026-08-11 10:22:09 +0200

    [rocm-jaxlib-v0.11.0] Clean up release branch 0.11.0 (#1099)
    * Set spawn strategy to local
    
    * Disable unnecessary workflows
    
    * Add bazel target and exclusion list
    
    * PR #46138: [ROCm] Fix gpu_cliques_test
    
    Imported from GitHub PR https://github.com/openxla/xla/pull/46138
    
    📝 Summary of Changes
    Make new subtest `SplitCliquesKeepsReorderedRanksOnCorrectExecutors` platform-agnostic.
    
    🎯 Justification
    In accordance with changes from https://github.com/openxla/xla/pull/44641
    
    🚀 Kind of Contribution
    Please remove what does not apply: 🐛 Bug Fix
    Copybara import of the project:
    
    --
    f650d7d33eb083a2426577e6f3b2638b8ae273ef by Milica Makevic <Milica.Makevic@amd.com>:
    
    Make SplitCliquesKeepsReorderedRanksOnCorrectExecutors platform agnostic
    
    Merging this change closes #46138
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/46138 from ROCm:mmakevic/fix_gpu_cliques_test f650d7d33eb083a2426577e6f3b2638b8ae273ef
    PiperOrigin-RevId: 953236947
    
    * Exclude failing tests
    
    * Disable failing tests

--- build_tools/rocm/run_xla_ci_build.sh
+++ build_tools/rocm/run_xla_ci_build.sh
@@ -23,6 +23,59 @@ TAG_FILTERS=$($SCRIPT_DIR/rocm_tag_filters.sh)
 
 mkdir -p /tf/pkg
 
+EXCLUDED_TESTS=(
+    "CollectivePipelineParallelismTestWithAndWithoutOpts/CollectivePipelineParallelismTest.PartiallyPipelinedAsyncSendRecvLoop/1"
+    "CublasFissionBackendTest.CublasFallbackForBf16Bf16F32Algorithm"
+    "TritonBackendTestSuite/TritonBackendTest.CostModelOptions_Filter/0"
+    "TritonBackendTestSuite/TritonBackendTest.CostModelOptions_TopFromDefault/0"
+    "TritonBackendTestSuite/TritonBackendTest.CostModelOptions_Filter/1"
+    "TritonBackendTestSuite/TritonBackendTest.CostModelOptions_TopFromDefault/1"
+    "TritonBackendTestSuite/TritonBackendTest.CostModelOptions_Combination/0"
+    "TritonBackendTestSuite/TritonBackendTest.CostModelOptions_Combination/1"
+    "GemmRewriteTest.CheckCustomCallHipblasLtBF16"
+    "ParameterizedGemmRewriteTest.GemmTypeCombinationCheck"
+    "FloatSupportTestWithCublas.MixedTypeDotIsNotUpcasted"
+    "BlasAlgorithmTest.Algorithm_BF16_BF16_F32_X6"
+    "BlasAlgorithmTest.Algorithm_BF16_BF16_F32_X3"
+    "BlasAlgorithmTest.Algorithm_BF16_BF16_F32"
+    "HostMemoryAllocateTest.Numa"
+    "F8E5M2Tests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_with_lhs_f8e5m2_rhs_f8e4m3fn_output_f32_from_cc_8_9_rocm_63_no_restriction_c_32_nc_32"
+    "F8E5M2Tests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_fast_accum_with_lhs_f8e5m2_rhs_f8e4m3fn_output_f32_from_cc_8_9_rocm_63_no_restriction_c_16_nc_2"
+    "F8E5M2Tests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_with_lhs_f8e5m2_rhs_f8e4m3fn_output_f32_from_cc_8_9_rocm_63_no_restriction_c_16_nc_2"
+    "F8E5M2Tests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_fast_accum_with_lhs_f8e5m2_rhs_f8e4m3fn_output_f32_from_cc_8_9_rocm_63_no_restriction_c_32_nc_32"
+    "F8E4M3FNTests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_fast_accum_with_lhs_f8e4m3fn_rhs_f8e4m3fn_output_f32_from_cc_8_9_rocm_63_no_restriction_c_16_nc_2"
+    "F8E4M3FNTests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_with_lhs_f8e4m3fn_rhs_f8e4m3fn_output_f32_from_cc_8_9_rocm_63_no_restriction_c_32_nc_32"
+    "F8E4M3FNTests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_with_lhs_f8e4m3fn_rhs_f8e4m3fn_output_f32_from_cc_8_9_rocm_63_no_restriction_c_16_nc_2"
+    "F8E4M3FNTests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_fast_accum_with_lhs_f8e4m3fn_rhs_f8e4m3fn_output_f32_from_cc_8_9_rocm_63_no_restriction_c_32_nc_32"
+    # mGPU tests
+    "CollectiveOpsTestE2E.CollectiveGroupAllReduceDifferentReplicaGroups"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_2GPUs_*"
+    "AsyncCollectiveOps/AsyncCollectiveOps.AsyncRaggedAllToAll_2GPUs_BF16/sync"
+    "AsyncCollectiveOps/AsyncCollectiveOps.AsyncRaggedAllToAll_2GPUs_BF16/async"
+    "AsyncCollectiveOps/AsyncCollectiveOps.AsyncRaggedAllToAll_2GPUs_BF16/sync_symmetric"
+    "AsyncCollectiveOps/AsyncCollectiveOps.AsyncRaggedAllToAll_2GPUs_BF16/async_symmetric"
+    # Cherry-pick https://github.com/openxla/xla/pull/46650 once it is merged and remove tests below
+    "GpuCollectivesTest.CreateRegisteredMemory"
+    "GpuCollectivesTest.CreateSymmetricMemory"
+    "GpuCollectivesTest.CreateWithMultipleIds"
+    "GpuCollectivesTest.SplitCommunicators"
+    "GpuCollectivesTest.PutAndWaitSignal"
+    # Cherry-pick https://github.com/openxla/xla/pull/46832 once it is approved and merged and remove tests below
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_2GPUs/sync_nccl_private"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_SeveralOps_2GPUs/async_nccl_private"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_SeveralOps_2GPUs/async_nccl_peer"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_2GPUs/async_nccl_peer"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_SeveralOps_2GPUs/sync_nccl_private"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_2GPUs/async_nccl_private"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_SeveralOps_2GPUs/async_one_shot_with_multi_gpu_barrier_with_nccl_private"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_SeveralOps_2GPUs/sync_nccl_peer"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_2GPUs/async_one_shot_with_multi_gpu_barrier_with_nccl_private"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_SeveralOps_2GPUs/sync_one_shot_with_multi_gpu_barrier_with_nccl_private"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_2GPUs/sync_one_shot_with_multi_gpu_barrier_with_nccl_private"
+    "RaggedAllToAllTest/RaggedAllToAllTest.RaggedAllToAll_2GPUs/sync_nccl_peer"
+
+)
+
 for arg in "$@"; do
     if [[ "$arg" == "--config=ci_multi_gpu" ]]; then
         TAG_FILTERS="${TAG_FILTERS},multi_gpu"
@@ -35,7 +88,6 @@ for arg in "$@"; do
     fi
 done
 
-SCRIPT_DIR=$(dirname $0)
 bazel --bazelrc="$SCRIPT_DIR/rocm_xla_ci.bazelrc" test \
     --build_tag_filters=$TAG_FILTERS \
     --test_tag_filters=$TAG_FILTERS \
@@ -48,4 +100,10 @@ bazel --bazelrc="$SCRIPT_DIR/rocm_xla_ci.bazelrc" test \
     --action_env=XLA_FLAGS="--xla_gpu_force_compilation_parallelism=16" \
     --test_output=errors \
     --run_under=//build_tools/rocm:parallel_gpu_execute \
-    "$@"
+    --test_filter=-$(
+        IFS=:
+        echo "${EXCLUDED_TESTS[*]}"
+    ) \
+    --color=yes \
+    "$@" \
+    //xla/...
