634e4ceef6e5b80e0cfb2baff9cabc3b1522205c
Steeve Morin <steeve@zml.ai>
2026-08-21 18:05:44 +0000

    [ROCm] Fix graph kernels with dynamic shared memory

--- xla/stream_executor/rocm/rocm_command_buffer.cc
+++ xla/stream_executor/rocm/rocm_command_buffer.cc
@@ -296,11 +296,6 @@ absl::StatusOr<GraphNodeHandle> RocmCommandBuffer::CreateKernelNode(
       const_cast<void**>(packed_args->argument_addresses().data());
   params.extra = nullptr;
 
-  if (shared_mem_bytes != 0) {
-    RETURN_IF_ERROR(
-        rocm_kernel.UpdateMaxDynamicSharedMemoryBytes(shared_mem_bytes));
-  }
-
   std::vector<hipGraphNode_t> deps = ToHipGraphHandles(dependencies);
 
   hipGraphNode_t node_handle = nullptr;
@@ -347,11 +342,6 @@ absl::Status RocmCommandBuffer::UpdateKernelNode(
   params.kernelParams =
       const_cast<void**>(packed_args->argument_addresses().data());
   params.extra = nullptr;
-
-  if (shared_mem_bytes != 0) {
-    RETURN_IF_ERROR(
-        rocm_kernel.UpdateMaxDynamicSharedMemoryBytes(shared_mem_bytes));
-  }
 
   return ToStatus(hipGraphExecKernelNodeSetParams(
                       exec_, ToHipGraphHandle(node_handle), &params),
